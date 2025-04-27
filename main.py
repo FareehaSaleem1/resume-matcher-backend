fd4550ecf9ef0d9fe526ab56df276b122adfc900
Fareeha Saleem <fareehasaleem934@gmail.com>
2025-04-27 22:34:19 +0500

    fixed1

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 origins = [
     "*",
     "http://localhost:3000",
-    "https://resume-matcher-frontend-your-vercel-url.vercel.app",
+    "https://resume-matcher-frontend-six.vercel.app/",
 ]
 
 app.add_middleware(
